0d5370733cefd231987d58dd23a1e066fc819b3c
Tu Nombre <alex.sanchez2900@gmail.com>
2021-02-16 11:01:52 +0100

    pub correct

--- scan_fake/src/node_scan_laser.cpp
+++ scan_fake/src/node_scan_laser.cpp
@@ -9,25 +9,11 @@
 using namespace std::chrono_literals;
 
 
-const int MAX_RANGE = 360;
-
-
-const int rand_max = 4.0;
-const int MAX_LECTURES = 100; //max number of lectures in our laser
-
 rclcpp::Node::SharedPtr node_sub = nullptr;
-
-//function to generate random values from 1.0 to 4.0
-//revise values!!! there are some values less than 1.0
-float floatRand() {
-  std::default_random_engine generator;
-  std::normal_distribution<double> distribution(4.0,1.0);
-  return distribution(generator);
-}
 
 void sub_callback(const sensor_msgs::msg::LaserScan::SharedPtr laser)
 {
-  RCLCPP_INFO(node_sub->get_logger(), "I heard: [%f]", laser->angle_max);
+  RCLCPP_INFO(node_sub->get_logger(), "I heard: [%f]", laser->ranges[0]);
 }
 
 
@@ -59,10 +45,15 @@ int main(int argc, char * argv[])
 
     std::vector<float> values;
 
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator (seed);
+    std::normal_distribution<float> distribution (4,1.0);
+
+
     //add 100 random lectures to the ranges array --> implement 3.6 each position
     for (int i = 0; i < 100; i++)
     {
-      values.push_back(floatRand()); 
+      values.push_back(distribution(generator)); 
     }
     
     //message data
@@ -72,9 +63,6 @@ int main(int argc, char * argv[])
     laser_message.angle_increment = M_PI/50;
     laser_message.scan_time = 1.0;
     laser_message.ranges = values;
-
-
-    RCLCPP_INFO(node_pub->get_logger(), "rand number [%f]", floatRand());
 
     publisher->publish(laser_message);
     RCLCPP_INFO(node_pub->get_logger(), "Publishing laser data...");
