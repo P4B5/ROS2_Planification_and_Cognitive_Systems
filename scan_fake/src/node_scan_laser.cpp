a8bb162859be17b03b790bb3a58175a03f601f15
marioesteban11 <48151661+marioesteban11@users.noreply.github.com>
2021-02-16 10:03:03 +0100

    cambios Node_scan
    Añadir angle_increment del laser y añadir distribución normal de 4 y 1

--- scan_fake/src/node_scan_laser.cpp
+++ scan_fake/src/node_scan_laser.cpp
@@ -5,6 +5,10 @@
 #include "cmath"
 #include "vector"
 
+#include <iostream>
+#include <string>
+#include <random>
+
 using namespace std::chrono_literals;
 
 
@@ -18,9 +22,9 @@ rclcpp::Node::SharedPtr node_sub = nullptr;
 
 //function to generate random values from 1.0 to 4.0
 //revise values!!! there are some values less than 1.0
-float floatRand() {
-  return rand_max * float(rand()) / (float(RAND_MAX) + 1.0);
-}
+/*float floatRand() {
+  return distribution(generator);
+}*/
 
 void sub_callback(const sensor_msgs::msg::LaserScan::SharedPtr laser)
 {
@@ -39,37 +43,43 @@ int main(int argc, char * argv[])
 
   node_sub = rclcpp::Node::make_shared("node_scan_sub");
   auto subscription = node_sub->create_subscription<sensor_msgs::msg::LaserScan>(
-    "laser_data", rclcpp::QoS(100).best_effort(), sub_callback); 
-    
+    "laser_data", rclcpp::QoS(100).best_effort(), sub_callback);
+
   /*
     band width = 1 lecture per second
     QoS Best effort --> laser data is admisable to lost casual info
   */
- 
+
   rclcpp::Rate loop_rate(1000ms); //1000ms = 1s = 1Hz
 
   rclcpp::executors::SingleThreadedExecutor executor;
   executor.add_node(node_pub);
   executor.add_node(node_sub);
-  
+
+
   while (rclcpp::ok()) {
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(4.0,1.0);
 
     std::vector<float> ranges;
 
     //add 100 random lectures to the ranges array --> implement 3.6 each position
     for (int i = 0; i < 100; i++)
     {
-      ranges.push_back(floatRand()); 
+      float number = distribution(generator);
+      ranges.push_back(number);
+      RCLCPP_INFO(node_pub->get_logger(), "rand number [%f]", number);
     }
-    
+
     //message data
     laser_message.angle_min = 0;      //min angle of laser in rad
     laser_message.angle_max = 2*M_PI; //max angle of laser in rad
+    laser_message.angle_increment = M_PI/50;
     laser_message.scan_time = 1.0;
     laser_message.ranges = ranges;
 
 
-    RCLCPP_INFO(node_pub->get_logger(), "rand number [%f]", floatRand());
+
 
     publisher->publish(laser_message);
     RCLCPP_INFO(node_pub->get_logger(), "Publishing laser data...");
