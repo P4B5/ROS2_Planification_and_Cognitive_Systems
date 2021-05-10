be51186f17ba0700f05f0fec627eaea080fc3363
alexs2900 <60138777+alexs2900@users.noreply.github.com>
2021-05-10 21:15:20 +0200

    Delete ROS2_Planification_and_Cognitive_Systems directory

--- ROS2_Planification_and_Cognitive_Systems/planning/tfd/launch_tfd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python2.7 $TFD_HOME/translate/translate.py [domain_name].pddl [problem_name].pddl
-$TFD_HOME/preprocess/preprocess < output.sas
-$TFD_HOME/search/search y Y a T 10 t 5 e r O 1 C 1 p $TFD_HOME/plan < output
